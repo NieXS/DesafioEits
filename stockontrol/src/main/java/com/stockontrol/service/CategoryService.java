0fb08b59985743550d786f16005b5135aad875a1
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-03-02 14:57:27 -0300

    CategoryService inicial

--- stockontrol/src/main/java/com/stockontrol/service/CategoryService.java
+++ stockontrol/src/main/java/com/stockontrol/service/CategoryService.java
@@ -1,5 +1,7 @@
 package com.stockontrol.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,11 +14,28 @@ public class CategoryService
 {
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Transactional
+	public List<Category> findAll()
+	{
+		return categoryRepository.findAll();
+	}
+	
+	@Transactional
+	public Category find(Long id)
+	{
+		return categoryRepository.findOne(id);
+	}
 
 	@Transactional
 	public Category save(Category category)
 	{
-		return categoryRepository.save(category);
+		return categoryRepository.saveAndFlush(category);
 	}
-
+	
+	@Transactional
+	public void delete(Category category)
+	{
+		categoryRepository.delete(category);
+	}
 }
