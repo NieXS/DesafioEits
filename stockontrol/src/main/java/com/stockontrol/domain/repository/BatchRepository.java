e83d2a097cd71127e4221303b3eb8b649c5b3c90
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-03-14 11:46:53 -0300

    Repositórios arrumados

--- stockontrol/src/main/java/com/stockontrol/domain/repository/BatchRepository.java
+++ stockontrol/src/main/java/com/stockontrol/domain/repository/BatchRepository.java
@@ -1,13 +1,10 @@
 package com.stockontrol.domain.repository;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
-
-import java.util.Date;
-import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import com.stockontrol.domain.entity.Batch;
@@ -16,16 +13,11 @@ import com.stockontrol.domain.entity.Product;
 @Repository("batchRepository")
 public interface BatchRepository extends JpaRepository<Batch, Long>, QueryDslPredicateExecutor<Batch>
 {
-	public Page<Batch> findAllByProduct(Product product, PageRequest page);
+	public Page<Batch> findAllByProduct(Product product, Pageable page);
 	
 	@Query("select b from Batch b where date(b.expiresAt) <= date(now()) and b.product.id = ?1")
-	public Page<Batch> listAllExpired(Long productId, PageRequest page);
+	public Page<Batch> listAllExpired(Long productId, Pageable page);
 	
 	@Query("select b from Batch b where date(b.expiresAt) > date(now()) and b.product.id = ?1")
-	public Page<Batch> listAllExpiring(Long productId, PageRequest page);
-	
-	// FIXME
-	@Query("select b from Batch b where (?1 is null or b.product.name like '%?1%') and "
-			+ "(?2 is null or b.identifier like '%?2%') and (?3 is null) and (?4 is null or b.product.id = ?4)")
-	public List<Batch> listAllByFilters(String productName, String identifier, Date maxExpirationDate, Long productId);
+	public Page<Batch> listAllExpiring(Long productId, Pageable page);
 }
--- stockontrol/src/main/java/com/stockontrol/domain/repository/CategoryRepository.java
+++ stockontrol/src/main/java/com/stockontrol/domain/repository/CategoryRepository.java
@@ -1,9 +1,7 @@
 package com.stockontrol.domain.repository;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
@@ -13,5 +11,5 @@ import com.stockontrol.domain.entity.Category;
 @Repository("categoryRepository")
 public interface CategoryRepository extends JpaRepository<Category, Long>, QueryDslPredicateExecutor<Category>
 {	
-	public Page<Category> findAllByNameIgnoreCaseContaining(String name, PageRequest page);
+	public Page<Category> findAllByNameIgnoreCaseContaining(String name, Pageable page);
 }
