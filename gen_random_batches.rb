cb31c627e11a973f9d0d5a598bc57a8247c79ad2
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-03-30 10:19:38 -0300

    Datas e valores menos absurdos

--- gen_random_batches.rb
+++ gen_random_batches.rb
@@ -16,10 +16,10 @@ file['products'].each do |name, data|
 	gen = Rubystats::NormalDistribution.new(90, 20)
 	gen.rng((25..55).sort_by{rand}[0]).map(&:to_i).each do |qtd|
 		identifier = "%.4s%.3d" % [(name.hash % 65536).to_s(16).upcase, seq]
-		base_date = Time.now + ((rand - 1)*60).days
+		base_date = Time.now + ((rand - 1)*40).days
 		manufactured_at = base_date - (rand*30).days
 		expires_at = base_date + (rand*60).days
-		puts %(<batches id="#{id}" product_id="#{data['id']}" user_id="#{user_id}" created_at="#{Time.now.strftime time_format}" updated_at="#{Time.now.strftime time_format}" manufactured_at="#{manufactured_at.strftime time_format}" expires_at="#{expires_at.strftime time_format}" identifier="#{identifier}" quantity="#{qtd}" />)
+		puts %(<batches id="#{id}" product_id="#{data['id']}" user_id="#{user_id}" created_at="#{base_date.strftime time_format}" updated_at="#{base_date.strftime time_format}" manufactured_at="#{manufactured_at.strftime time_format}" expires_at="#{expires_at.strftime time_format}" identifier="#{identifier}" quantity="#{qtd}" />)
 		seq += 1
 		id += 1
 	end
