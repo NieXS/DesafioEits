a75b0308a7386d01209407a6b5160b782737430f
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-03-30 09:52:14 -0300

    Imports organizados

--- stockontrol/src/test/java/com/stockontrol/test/domain/service/UserServiceTests.java
+++ stockontrol/src/test/java/com/stockontrol/test/domain/service/UserServiceTests.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 
-import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.stockontrol.domain.entity.User;
 import com.stockontrol.domain.service.UserService;
 import com.stockontrol.test.domain.AbstractIntegrationTests;
