4796ff76491de6af97aed95bb3abafadffefd4cc
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-04-01 14:25:40 -0300

    Datas certas no REST

--- stockontrol/src/main/java/com/stockontrol/application/configuration/WebConfig.java
+++ stockontrol/src/main/java/com/stockontrol/application/configuration/WebConfig.java
@@ -1,12 +1,22 @@
 package com.stockontrol.application.configuration;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.mvc.SimpleControllerHandlerAdapter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 @EnableWebMvc
@@ -25,9 +35,30 @@ public class WebConfig extends WebMvcConfigurerAdapter
 		configurer.enable();
 	}
 	
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters)
+	{
+		converters.add(jacksonConverter());
+	}
+	
 	@Bean
 	public SimpleControllerHandlerAdapter simpleControllerHandlerAdapter()
 	{
 		return new SimpleControllerHandlerAdapter();
+	}
+	
+	@Bean
+	public ObjectMapper objectMapper()
+	{
+		return Jackson2ObjectMapperBuilder.json()
+				.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+				.modules(new JavaTimeModule())
+				.build();
+	}
+	
+	@Bean
+	public MappingJackson2HttpMessageConverter jacksonConverter()
+	{
+		return new MappingJackson2HttpMessageConverter(objectMapper());
 	}
 }
