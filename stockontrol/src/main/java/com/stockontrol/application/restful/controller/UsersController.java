9cba9489cb18aa6afc24600cefc282d21ab7875c
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-04-01 15:25:18 -0300

    import redundante

--- stockontrol/src/main/java/com/stockontrol/application/restful/controller/UsersController.java
+++ stockontrol/src/main/java/com/stockontrol/application/restful/controller/UsersController.java
@@ -1,6 +1,5 @@
 package com.stockontrol.application.restful.controller;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.PathVariable;
