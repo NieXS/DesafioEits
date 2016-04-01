e6bdb3312b0f4e300e186e610758f1f8eaccb7e9
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-04-01 14:27:51 -0300

    import redundante

--- stockontrol/src/main/java/com/stockontrol/application/configuration/WebConfig.java
+++ stockontrol/src/main/java/com/stockontrol/application/configuration/WebConfig.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
