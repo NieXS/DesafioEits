197ad3a28241af3c8ee58356a667520f44480b3b
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-04-01 16:23:27 -0300

    import supérfluo removido

--- stockontrol/src/main/java/com/stockontrol/domain/service/UserService.java
+++ stockontrol/src/main/java/com/stockontrol/domain/service/UserService.java
@@ -10,7 +10,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.stockontrol.application.aspect.EmailerAspect;
 import com.stockontrol.domain.entity.QUser;
 import com.stockontrol.domain.entity.User;
 import com.stockontrol.domain.repository.UserRepository;
