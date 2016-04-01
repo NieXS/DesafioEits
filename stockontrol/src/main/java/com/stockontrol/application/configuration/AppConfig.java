4c0b15c2d2cfeb1be16c352aa9fd7c0010845a1d
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-04-01 11:57:30 -0300

    AppConfig

--- stockontrol/src/main/java/com/stockontrol/application/configuration/AppConfig.java
+++ stockontrol/src/main/java/com/stockontrol/application/configuration/AppConfig.java
@@ -1,14 +1,20 @@
 package com.stockontrol.application.configuration;
 
+import java.time.Clock;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource({"classpath:/security.xml", "classpath:/dwr.xml", "classpath:/email.xml",
-		"classpath:/appConfig.xml" })
-@ComponentScan({"com.stockontrol.application.*", "com.stockontrol.domain.*"})
+@ImportResource({"classpath:/dwr.xml", "classpath:/email.xml" })
+@ComponentScan({ "com.stockontrol.application.*", "com.stockontrol.domain.*" })
 public class AppConfig
 {
-	
+	@Bean
+	public Clock clock()
+	{
+		return Clock.systemDefaultZone();
+	}
 }
