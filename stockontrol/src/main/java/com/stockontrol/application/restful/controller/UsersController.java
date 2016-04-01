b1e24409781b0e00a0c87674a159a0196f1ab392
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-04-01 15:24:38 -0300

    Rotas REST finalizadas

--- stockontrol/src/main/java/com/stockontrol/application/restful/controller/UsersController.java
+++ stockontrol/src/main/java/com/stockontrol/application/restful/controller/UsersController.java
@@ -1,7 +1,10 @@
 package com.stockontrol.application.restful.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +22,7 @@ public class UsersController
 	private UserService userService;
 
 	@RequestMapping(value = { "", "/" }, method = RequestMethod.GET)
-	public Iterable<User> listUsers(@RequestParam(name = "filter", required = false) String filter,
+	public Page<User> index(@RequestParam(name = "filter", required = false) String filter,
 			@RequestParam(name = "active", required = false) Boolean active,
 			@RequestParam(name = "profile", required = false) User.Profile profile,
 			@RequestParam(name = "page", defaultValue = "0") int page)
@@ -29,11 +32,57 @@ public class UsersController
 		pageRequest.setSize(20);
 		return userService.listAllByFilters(filter, active, profile, pageRequest);
 	}
+	
+	@RequestMapping(value = { "", "/" }, method = RequestMethod.POST)
+	public User create(@RequestBody User user)
+	{
+		user.setActive(true);
+		return userService.insert(user);
+	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public String getUser(@PathVariable String id)
+	public User show(@PathVariable Long id)
 	{
-		String result = "Usuário com id " + id;
-		return result;
+		return userService.find(id);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
+	public User update(@RequestBody User sentUser, @PathVariable Long id)
+	{
+		User user = userService.find(id);
+		
+		if(sentUser.getFullName() != null)
+		{
+			user.setFullName(sentUser.getFullName());
+		}
+		
+		if(sentUser.getEmail() != null)
+		{
+			user.setEmail(sentUser.getEmail());
+		}
+		
+		if(sentUser.getPassword() != null)
+		{
+			user.setPassword(sentUser.getPassword());
+		}
+		
+		if(sentUser.getProfile() != null)
+		{
+			user.setProfile(sentUser.getProfile());
+		}
+		
+		return userService.save(user);
+	}
+	
+	@RequestMapping(value = "/{id}/activate", method = RequestMethod.POST)
+	public User activate(@PathVariable Long id)
+	{
+		return userService.activate(userService.find(id));
+	}
+	
+	@RequestMapping(value = "/{id}/deactivate", method = RequestMethod.POST)
+	public User deactivate(@PathVariable Long id)
+	{
+		return userService.deactivate(userService.find(id));
 	}
 }
